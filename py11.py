abd6e30c6c8227849ce1caf84bf1177b24c172f1
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-20 23:38:23 +0530

    loop control

--- py11.py
+++ py11.py
@@ -4,14 +4,14 @@
 
 #let's create user prompt
 
-#rows = int(input("how many rows?:"))
-#columns = int(input("how many colmns?:"))
-#symbols = input("what is the symbol to use?:")
+rows = int(input("how many rows?:"))
+columns = int(input("how many colmns?:"))
+symbols = input("what is the symbol to use?:")
 
-#for i in range(rows):
-#    for j in range(columns):
-#        print(symbols,end="")
-#   print()
+for i in range(rows):
+   for j in range(columns):
+        print(symbols,end="")
+   print()
 
 # loop control statements(break,continue,pass)
 
@@ -21,45 +21,45 @@
 # continue = skips to next iteration of the loop
 # pass = does nothing, acts as a place holder!
 
-#while True:
-#    name = input("Enter your name:")
-#    if name !="":
-#        break
+while True:
+    name = input("Enter your name:")
+    if name !="":
+        break
 
 
-#phone_number = "180-224-6786"
-#for i in phone_number:
-#    if i =="-":
-#        continue
-#    print(i,end="")
+phone_number = "180-224-6786"
+for i in phone_number:
+    if i =="-":
+        continue
+    print(i,end="")
 
-#name = "xiang_tzu_xian_zhou"
-#for i in name:
-#    if i =="_":
-#        continue
-#    print(i,end="")
+name = "xiang_tzu_xian_zhou"
+for i in name:
+    if i =="_":
+        continue
+    print(i,end="")
 
 #pass statement!
 
-#for i in range(10,20):
-#    if i == 13:
-#        pass
-#    else:
-#         print(i)
+for i in range(10,20):
+    if i == 13:
+        pass
+    else:
+         print(i)
 
-#name = "xian_tzu_xian_zhou"
-#for i in name:
- #   if i == "_":
-  #      continue
-   # print(i,end="")
+name = "xian_tzu_xian_zhou"
+for i in name:
+    if i == "_":
+        continue
+    print(i,end="")
 
-#while True:
-#    name = input("enter your name:")
-#    if name!="":
-#        break
+while True:
+    name = input("enter your name:")
+    if name!="":
+        break
 
-#for i in range(10):
- #   if i == 4:
-  #      pass
-   # else:
-    #    print(i)
+for i in range(10):
+    if i == 4:
+        pass
+    else:
+        print(i)
